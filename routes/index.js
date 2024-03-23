6d6dcdeb074adc64dfff0115ea40cbf85c5ee59a
JoseAlz <joseufox@hotmail.com>
2024-03-22 22:30:22 -0300

    odin express

---
+++ routes/index.js
@@ -0,0 +1,39 @@
+var express = require("express");
+var router = express.Router();
+
+const messages = [
+  {
+    text: "Developers rules!",
+    user: "Dev",
+    added: new Date(),
+  },
+  {
+    text: "Yes, we will win!",
+    user: "user",
+    added: new Date(),
+  },
+];
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Message board", messages: messages });
+  console.log("The index data:", req.body);
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("form", { title: "Message board" });
+});
+
+router.post("/new", function (req, res) {
+  const formData = req.body;
+  console.log("the form data:", formData);
+  messages.push({text: formData.messageText, user: formData.authorName, added: new Date()});
+  res.redirect("/");
+  // res.redirect("message", {
+  //   title: "New message",
+  //   formData: formData,
+  //   added: new Date(),
+  // });
+});
+
+module.exports = router;
